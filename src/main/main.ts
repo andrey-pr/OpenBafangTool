2ed0e9c629b6569afb22f5e7b6982583998aa89d
Andrey <>
2024-06-03 16:12:13 +0200

    Simplified launch procedure

--- src/main/main.ts
+++ src/main/main.ts
@@ -40,24 +40,7 @@ if (isDebug) {
     require('electron-debug')();
 }
 
-const installExtensions = async () => {
-    const installer = require('electron-devtools-installer');
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
-    const extensions = ['REACT_DEVELOPER_TOOLS'];
-
-    return installer
-        .default(
-            extensions.map((name) => installer[name]),
-            forceDownload,
-        )
-        .catch(console.log);
-};
-
 const createWindow = async () => {
-    if (isDebug) {
-        await installExtensions();
-    }
-
     const RESOURCES_PATH = app.isPackaged
         ? path.join(process.resourcesPath, 'assets')
         : path.join(__dirname, '../../assets');
@@ -92,6 +75,7 @@ const createWindow = async () => {
 
     mainWindow.on('closed', () => {
         mainWindow = null;
+        app.quit();
     });
 
     const menuBuilder = new MenuBuilder(mainWindow);
